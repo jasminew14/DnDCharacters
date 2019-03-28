6315f7131924601067a5064d5d0a922f36122995
jasminew14 <jasminewblog@gmail.com>
2019-03-27 22:25:29 -0700

    First Version of Menu

---
+++ Main.py
@@ -0,0 +1,23 @@
+
+print("Enter A to create character")
+print("Enter B to view file or print characters")
+print("Enter C to have characters fight")
+print("Enter E to quit")
+choice = input("Enter choice: ")
+choice.upper()
+while True:
+    print("Enter A to create character")
+    print("Enter B to view file or print characters")
+    print("Enter C to have characters fight")
+    print("Enter E to quit")
+    choice = input("Enter choice: ")
+    if choice == "A":
+        print("worked")
+    elif choice == "B":
+        print("worked")
+    elif choice == "C":
+        print("worked")
+    elif choice == "E":
+        print("worked")
+        break
+
