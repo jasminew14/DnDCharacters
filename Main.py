107957e11a5918241f5b971436cd12898e205a44
jasminew14 <13566318+jasminew14@users.noreply.github.com>
2019-05-13 20:34:18 -0700

    Main.py
    Main now uses a text file to save object info.

--- Main.py
+++ Main.py
@@ -1,50 +1,46 @@
 from Character import Character
 import pickle
+import json
 print("Welcome to your character creator")
 
-"""def save(self, fileName = None):
-    if fileName!= None:
-        self.fileName = fileName
-    elif self.fileName == None:
-        return
-    fileObj = open(self. fileName, "wb")
-    for account in self.accounts.values():
-        pickle.dump(account, fileObj)
-    fileObj.close()
 
-def load(self, fileName = None):
-    self.accounts = {}
-    self.fileName = fileName
-    if fileName != None:
-        fileObj = open(fileName, "rb")
-        while True:
-            try:
-                account = pickle.load(fileObj)
-                self.add(account)
-            except EOFError:
-                fileObj.close()
-                break"""
 
 def save(objectName,fileName):
     object = objectName
     file = open(fileName, 'wb')
     pickle.dump(object, file)
     file.close()
+
 def load(fileName):
     filehandler = open(fileName, 'rb')
     object = pickle.load(filehandler)
     print(object)
     filehandler.close()
+def saveJson(objectName,fileName):
+    object = objectName
+    file = open(fileName, 'wb')
+    pickle.dump(object, file)
+    file.close()
+
+def loadJson(fileName):
+    filehandler = open(fileName, 'rb')
+    object = pickle.load(filehandler)
+    print(object)
+    filehandler.close()
+
+
 def main():
     while True:
         characterList = []
         print("Enter A to create character")
         print("Enter B to view file or print characters")
         print("Enter C to have characters fight")
-        print("Enter E to quit")
+        print("Enter D to quit")
         choice = input("Enter choice: ").upper()
         if choice == "A":
-            name = input("Enter a character name: ").lower
+            name = input("Enter a character name: ")
+            name = name.lower()
+            name = name.capitalize()
             objectName = Character(name)
             """if len(characterList)>0:
                 while True:
@@ -56,17 +52,31 @@ def main():
                             else:
                                 False"""
             print(objectName)
-            print("Do you want to save charcter? ")
+            print("Do you want to save character? ")
             print("Enter A or press any key to quit")
             choice = input("Enter choice: ").upper()
             if choice == "A":
-                save(objectName, name)
+                f = open('Saves.txt','w')
+                f.write(objectName.getattributes())
+                f.close()
+                print("Character was saved")
         elif choice == "B":
-            filename = input("Enter name of the character you want to load: ").lower
+            filename = input("Enter name of the character you want to load: ")
+            filename = filename.lower()
+            filename = filename.capitalize()
             load(filename)
         elif choice == "C":
-            print("worked")
-        elif choice == "E":
+            """character1 = input("Choose character 1: ")
+            character2 = input("Choose character 2: ")
+            if character1 == character2:
+                print("You mst choose two characters")
+            else:
+                character1 = loadCharacter1(character1)
+                character2 = loadCharacter2(character2)"""
+            filename = input("Enter name of the character you want to load: ")
+            filename = filename.lower()
+            filename = filename.capitalize()
+        elif choice == "D":
             print("Bye Bye")
             break
         else:
