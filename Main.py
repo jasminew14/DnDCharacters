9f1526989cceb88d6ecd7f3704235009527cf031
jasminew14 <13566318+jasminew14@users.noreply.github.com>
2019-05-16 14:24:05 -0700

    Main.py
    Deleted old comments and load and save definitions

--- Main.py
+++ Main.py
@@ -1,39 +1,14 @@
 from Character import Character
-import pickle
-import json
+from Management import Management
+
+o = Management
 print("Welcome to your character creator")
-
-
-
-def save(objectName,fileName):
-    object = objectName
-    file = open(fileName, 'wb')
-    pickle.dump(object, file)
-    file.close()
-
-def load(fileName):
-    filehandler = open(fileName, 'rb')
-    object = pickle.load(filehandler)
-    print(object)
-    filehandler.close()
-def saveJson(objectName,fileName):
-    object = objectName
-    file = open(fileName, 'wb')
-    pickle.dump(object, file)
-    file.close()
-
-def loadJson(fileName):
-    filehandler = open(fileName, 'rb')
-    object = pickle.load(filehandler)
-    print(object)
-    filehandler.close()
 
 
 def main():
     while True:
-        characterList = []
         print("Enter A to create character")
-        print("Enter B to view file or print characters")
+        print("Enter B to print characters")
         print("Enter C to have characters fight")
         print("Enter D to quit")
         choice = input("Enter choice: ").upper()
@@ -41,39 +16,21 @@ def main():
             name = input("Enter a character name: ")
             name = name.lower()
             name = name.capitalize()
-            objectName = Character(name)
-            """if len(characterList)>0:
-                while True:
-                    for character in len(characterList):
-                        if characterList[character] == name:
-                            name = input("Enter a new character name: ")
-                            if characterList[character] == name:
-                                True
-                            else:
-                                False"""
-            print(objectName)
+            objectname = Character(name)
+            print(objectname)
             print("Do you want to save character? ")
             print("Enter A or press any key to quit")
             choice = input("Enter choice: ").upper()
             if choice == "A":
-                f = open('Saves.txt','w')
-                f.write(objectName.getattributes())
-                f.close()
-                print("Character was saved")
+                if o.save(None,name):
+                    f = open('Saves.txt', 'a')
+                    f.write(objectname.getattributes())
+                    f.close()
+                else:
+                    continue
         elif choice == "B":
-            filename = input("Enter name of the character you want to load: ")
-            filename = filename.lower()
-            filename = filename.capitalize()
-            load(filename)
+            o.showcharacters(None)
         elif choice == "C":
-            """character1 = input("Choose character 1: ")
-            character2 = input("Choose character 2: ")
-            if character1 == character2:
-                print("You mst choose two characters")
-            else:
-                character1 = loadCharacter1(character1)
-                character2 = loadCharacter2(character2)"""
-            filename = input("Enter name of the character you want to load: ")
             filename = filename.lower()
             filename = filename.capitalize()
         elif choice == "D":
