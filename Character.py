6984752cb9836b7bbf2f042f5d4626ec9082190f
jasminew14 <13566318+jasminew14@users.noreply.github.com>
2019-05-19 21:31:12 -0700

    Character
    A cleaned version of Character.py that includes comments

--- Character.py
+++ Character.py
@@ -1,9 +1,9 @@
 import random
-import pickle
+
 
 class Character(object):
 
-    def __init__(self, name):
+    def __init__(self, name):  # # Creating a character with random attributes
         self.name = name
         races = ["Elf", "Dwarf", "Orc", "Human", "Gnome", "Dragonborn"]
         self.race = random.choice(races)
@@ -14,24 +14,11 @@ class Character(object):
         self.health = random.randint(50, 100)
         self.characterList = []
 
-    """def nameDuplicate(self):
-        if len(self.characterList) > 0:
-            while True:
-                for character in len(self.characterList):
-                    if self.characterList[character] == self.characterID():
-                        if self.characterList[character] == self.name:
-                            True
-                        else:
-
-                            False"""
-    def getattributes(self):
+    def getattributes(self):   # # Making a method that returns attribute in a one line separated by commas
         return self.name + "," + self.race + "," + str(self.health) + "," + str(self.luck) + "," +\
                str(self.strength) + "," + str(self.intelligence) + "," + str(self.dexterity)
 
-    def __str__(self):
+    def __str__(self):   # # Printing character so the user can see
         return "You are " + self.name + " the " + self.race + "\n Your stats are \n Strength: " + str(self.strength) + \
                "\n Intelligence: " + str(self.intelligence) + "\n Luck: " + str(self.luck) + \
                "\n Dexterity: " + str(self.dexterity) + "\n Health: " + str(self.health)
-
-
-
